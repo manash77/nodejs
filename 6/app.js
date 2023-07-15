11b44ee32f95bd58abcdeb5fc8c8f6f04ddbf4f8
manash7 <56928638+manash7@users.noreply.github.com>
2023-07-15 18:12:52 +0530

    Request response are added for home about and node

--- 6/app.js
+++ 6/app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("Manash Gupta")
+    const url = req.url;
+    if (url === "/home") {
+        res.write('<html>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+    }
+    if (url === "/about") {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to About us page</h1></body>')
+        res.write('</html>')
+    }
+    if (url === "/node") {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to My Node js project page</h1></body>')
+        res.write('</html>')
+    }
+    res.end();
 })
 
 server.listen(4000)
