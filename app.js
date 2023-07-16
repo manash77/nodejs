f224eb260d523657fe27fde149c02fe3993f82ad
manash7 <56928638+manash7@users.noreply.github.com>
2023-07-16 18:01:50 +0530

    express js is added

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const express = require('express');
+
+const server = http.createServer();
+
+const app = express();
+
+server.listen(4000)     
---
+++ routes.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/") {
+        fs.readFile('message.txt', { encoding: 'utf-8' }, (err, data) => {
+            if (err) console.log(err)
+            res.write('<html>')
+            res.write(`<h3>${data}<h3>`)
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>')
+            res.write('</html>')
+            return res.end();
+        })
+
+    }
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, () => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end();
+            })
+        });
+    }
+
+}
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler:requestHandler,
+//     someText:"Some Hard Coded text"
+// }
+
+module.exports.handler = requestHandler;
+module.exports.someText = "Some Hard Coded text";
