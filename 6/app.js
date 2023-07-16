2bc34498e9afa4ccba5b7d0a5cdf241c63e80148
manash7 <56928638+manash7@users.noreply.github.com>
2023-07-16 09:14:46 +0530

    Added Message and Input box

--- 6/app.js
+++ 6/app.js
@@ -1,23 +1,36 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     const url = req.url;
-    if (url === "/home") {
-        res.write('<html>')
-        res.write('<body><h1>Welcome home</h1></body>')
-        res.write('</html>')
+    const method = req.method;
+
+    if (url === "/") {
+        fs.readFile('message.txt', { encoding: 'utf-8' }, (err, data) => {
+            if (err) console.log(err)
+            res.write('<html>')
+            res.write(`<h3>${data}<h3>`)
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>')
+            res.write('</html>')
+            return res.end();
+        })
+
     }
-    if (url === "/about") {
-        res.write('<html>')
-        res.write('<body><h1>Welcome to About us page</h1></body>')
-        res.write('</html>')
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, () => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end();
+            })
+        });
     }
-    if (url === "/node") {
-        res.write('<html>')
-        res.write('<body><h1>Welcome to My Node js project page</h1></body>')
-        res.write('</html>')
-    }
-    res.end();
+
 })
 
 server.listen(4000)
