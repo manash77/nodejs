eac876e9d174b40dd38a41e2c32de5ab6034e9d2
manash7 <56928638+manash7@users.noreply.github.com>
2023-07-17 22:46:50 +0530

    body parser is added and post method is added

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log("in the middleware");
-    next();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
 })
-app.use((req, res, next) => {
-    console.log("in Another middleware");
-    res.send({})
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><input type="number" name="size"/><button type="submit">Send</button></form>')
+})
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello From Express</h1>')
 })
 app.listen(3000)  
