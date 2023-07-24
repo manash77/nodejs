e62c302c3ba754ce49a3c6ef9a87552d1e551d16
manash7 <56928638+manash7@users.noreply.github.com>
2023-07-24 13:19:41 +0530

    delete DOMLoaded Function Added

--- frontend/main.js
+++ frontend/main.js
@@ -1,33 +1,70 @@
 const form = document.getElementById('myForm');
+const list = document.getElementById('list');
 
-function onSubmit(e){
+function onSubmit(e) {
     e.preventDefault();
-  const username = e.target.username.value;
-  const email = e.target.email.value;
-  const phone = e.target.phone.value;
+    const username = e.target.username.value;
+    const email = e.target.email.value;
+    const phone = e.target.phone.value;
 
-  const obj = {
-    username,
-    email,
-    phone
-  }
-  addUser(obj)
-  form.reset();
+    const obj = {
+        username,
+        email,
+        phone
+    }
+    addUser(obj)
+    form.reset();
 }
 
-async function getAllUser(){
-
-}
-async function deleteUser(){
-
-}
-async function addUser(user){
+async function addUser(user) {
     try {
         const response = await axios.post('http://localhost:8000/user/add-user', user);
-        if (response.status === 201) {
-          await getAllUser();
-        }
-      } catch (error) {
+        console.log("add-user",response);
+        if (response.status === 200) {
+            await getAllUser();
+          }
+    } catch (error) {
         console.error("Error While Saving Data", error);
-      }
+    }
 }
+
+async function deleteUser(id) {
+    try {
+        const response = await axios.post('http://localhost:8000/user/delete-user', { id: id })
+        getAllUser()
+    } catch (error) {
+        console.error("Error While Deleting Data", error);
+    }
+}
+
+async function getAllUser() {
+    try {
+        const response = await axios.get('http://localhost:8000/user/get-user');
+        renderData(response.data);
+    } catch (error) {
+        console.error("Error While getting Data Data", error);
+    }
+}
+
+function renderData(users) {
+    list.innerHTML = '';
+    users.forEach(user => {
+        let Element = document.createElement('li');
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = 'delete';
+        Element.innerHTML = user.username + "  -  " + user.email + "  -  " + user.phone;
+        Element.classList = 'list-group-item'
+        deleteButton.classList = 'btn btn-danger mx-4';
+        deleteButton.addEventListener("click", () => deleteUser(user.id));
+        Element.appendChild(deleteButton);
+        list.appendChild(Element);
+    });
+}
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+        await getAllUser();
+    } catch (error) {
+        alert("Api Is Expired")
+        console.error("Error While Loading Data When Screen is Loading", error);
+    }
+})
