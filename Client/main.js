f56f1458d1aec366d84e662019651e641a33ab8f
manash7 <56928638+manash7@users.noreply.github.com>
2023-07-29 01:01:08 +0530

    update user validation added

--- Client/main.js
+++ Client/main.js
@@ -1,11 +1,20 @@
 const form = document.getElementById('signUpForm');
+form.addEventListener('submit',onSubmit);
 
 //Form Submit Function
 function onSubmit(e) {
   e.preventDefault();
+  console.log(e);
+  // const list = form.classList();
+  form.className = "was-validated";
   const name = e.target.name.value;
   const email = e.target.email.value;
   const password = e.target.password.value;
+
+  if(name ==="" || email === "" || password === ""){
+    alert('Please Fill Form')
+    return 
+  }
 
   const obj = {
     name,
@@ -20,7 +29,11 @@ function onSubmit(e) {
 async function saveData(order) {
   try {
     const response = await axios.post('http://localhost:8000/users/signup', order);
-    if (response.status === 200) {
+    console.log(response);
+    if (response.data.userExists) {
+      alert('User Already Exits');
+    }
+    if (response.status === 201) {
       alert('data Added')
     }
   } catch (error) {
--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,19 +1,51 @@
 const Users = require('../models/users');
 
-exports.createUser = (req, res, next) => {
+exports.createUser = async (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
-    Users.create({
-        name: name,
-        email: email,
-        password: password
-    })
-        .then((response) => {
-            return res.json(response)
-        })
-        .catch((err) => {
-            console.error(err);
-        })
+    const [user, created] = await Users.findOrCreate({
+        where: { email:email },
+        defaults: {
+            name,
+            email,
+            password
+        }
+    });
+
+    console.log(created);
+    if (created) {
+       return res.status(201).json(user)
+    } else {
+        return res.json({userExists:true})
+    }
+
+    // Users.findOne({ where: { email: email } })
+    //     .then((user) => {
+    //         console.log(typeof user.email);
+    //         if (user.email == email) {
+    //             console.log("if works ");
+    //             res.status = 409;
+    //             return res.json()
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         return res.json(err)
+    //     })
+    //     .then(() => {
+    //         Users.create({
+    //             name,
+    //             email,
+    //             password
+    //         })
+    //             .then((response) => {
+    //                 return res.json(response)
+    //             })
+    //             .catch((err) => {
+    //                 return res.json(err)
+    //             })
+    //     })
+
+
 }
